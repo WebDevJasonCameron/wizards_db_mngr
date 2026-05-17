520b182b6fe302f49e200eaceb775603a290e659
Jason Cameron <dev.jasoncameron@gmail.com>
2026-05-16 20:45:19 -0500

    additional changes to db

--- sql/00_create_table_sql/10_ct_spells.sql
+++ sql/00_create_table_sql/10_ct_spells.sql
@@ -3,7 +3,6 @@ CREATE SEQUENCE IF NOT EXISTS spell_seq start 1;
 CREATE TABLE IF NOT EXISTS spells(
                         id BIGINT NOT NULL DEFAULT nextval('spell_seq'),
                         spell_name VARCHAR(255) NOT NULL UNIQUE,
-                        spell_ttrpg BIGINT,
                         spell_level VARCHAR(255),
                         spell_casting_time VARCHAR(255),
                         spell_range_area VARCHAR(255),
@@ -21,10 +20,6 @@ CREATE TABLE IF NOT EXISTS spells(
                         spell_created_at TIMESTAMP NOT NULL DEFAULT NOW(),
                         spell_updated_at TIMESTAMP,
 
-                        spell_source_id BIGINT,
-
-                        FOREIGN KEY (spell_ttrpg) REFERENCES ttrpgs(id),
-                        FOREIGN KEY (spell_source_id) REFERENCES sources(id),
                         PRIMARY KEY (id)
 );
 
--- sql/00_create_table_sql/38_ct_spell_sources.sql
+++ sql/00_create_table_sql/38_ct_spell_sources.sql
@@ -1,7 +1,7 @@
 CREATE SEQUENCE IF NOT EXISTS spell_sources_seq start 1;
 
 CREATE TABLE IF NOT EXISTS spell_sources (
-                                             id BIGINT NOT NULL DEFAULT nextval('spell_sources_seq'),
+                                           id BIGINT NOT NULL DEFAULT nextval('spell_sources_seq'),
                                            spells_spell_id BIGINT,
                                            sources_source_id BIGINT,
                                            spell_source_page VARCHAR(20),
@@ -12,5 +12,5 @@ CREATE TABLE IF NOT EXISTS spell_sources (
                                            PRIMARY KEY (id)
 );
 
-ALTER SEQUENCE spell_sources_seq OWNED BY spell_sources.spell_sources_id;
+ALTER SEQUENCE spell_sources_seq OWNED BY spell_sources.id;
 ALTER SEQUENCE spell_sources_seq RESTART WITH 1;
